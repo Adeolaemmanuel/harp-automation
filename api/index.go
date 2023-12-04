0762572fcaa0b24957cf0d89b02a019e99de5c80
Adeola Emmanuel <adeolaemmanuel006@gmail.com>
2023-12-04 23:23:49 +0100

    update on exported func

--- api/index.go
+++ api/index.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"harp-automation.com/com/modules/automation"
 )
 
 var autoRouter = automation.Automation{}
 
-func Handler () {
+func Handler(w http.ResponseWriter, r *http.Request) {
 	server := gin.Default()
 
 	autoRouter.AutomationRouter(server.Group("/api/v1/automation"))
