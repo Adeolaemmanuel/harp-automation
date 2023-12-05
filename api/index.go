658843c7ef110419fc31024b2e01e1d8965abaeb
Adeola Emmanuel <adeolaemmanuel006@gmail.com>
2023-12-05 08:27:52 +0100

    update on changes due to gin conversion

--- api/index.go
+++ api/index.go
@@ -1,4 +1,4 @@
-package main
+package handler
 
 import (
 	"net/http"
@@ -19,9 +19,9 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	server.ServeHTTP(w, r)
 }
 
-func main() {
-	server := gin.Default()
+// func main() {
+// 	server := gin.Default()
 
-	autoRouter.AutomationRouter(server.Group("/api/v1/automation"))
-	server.Run(":3000")
-}
+// 	autoRouter.AutomationRouter(server.Group("/api/v1/automation"))
+// 	server.Run(":3000")
+// }
